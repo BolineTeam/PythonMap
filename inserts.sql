c2d7e213915bb46beea3a2a83c0e52c11e1a1973
Joseph Efrain Camargo <efrain.camargo@cbqasolutions.com>
2023-09-28 22:11:24 -0600

    add: inserts in distance_store

--- inserts.sql
+++ inserts.sql
@@ -21,3 +21,33 @@ INSERT INTO store (name, lat, `long`) VALUES ('Tienda I', 47.6808, 54.874);
 
 -- Inserción de prueba en la tabla Store para la Tienda J
 INSERT INTO store (name, lat, `long`) VALUES ('Tienda J', 47.6987, 54.892);
+
+
+-- Inserción de prueba en la tabla DistanceStore para la Tienda 1
+INSERT INTO distance_store (idStore1, idStore2, distance) VALUES (1, 2, 3.0);
+
+-- Inserción de prueba en la tabla DistanceStore para la Tienda 1
+INSERT INTO distance_store (idStore1, idStore2, distance) VALUES (1, 3, 5.0);
+
+-- Inserción de prueba en la tabla DistanceStore para la Tienda 1
+INSERT INTO distance_store (idStore1, idStore2, distance) VALUES (1, 4, 2.0);
+
+-- Inserción de prueba en la tabla DistanceStore para la Tienda 1
+INSERT INTO distance_store (idStore1, idStore2, distance) VALUES (1, 5, 4.5);
+
+-- Inserción de prueba en la tabla DistanceStore para la Tienda 1
+INSERT INTO distance_store (idStore1, idStore2, distance) VALUES (1, 6, 6.2);
+
+-- Inserción de prueba en la tabla DistanceStore para la Tienda 1
+INSERT INTO distance_store (idStore1, idStore2, distance) VALUES (1, 7, 8.1);
+
+-- Inserción de prueba en la tabla DistanceStore para la Tienda 1
+INSERT INTO distance_store (idStore1, idStore2, distance) VALUES (1, 8, 1.8);
+
+-- Inserción de prueba en la tabla DistanceStore para la Tienda 1
+INSERT INTO distance_store (idStore1, idStore2, distance) VALUES (1, 9, 7.3);
+
+-- Inserción de prueba en la tabla DistanceStore para la Tienda 1
+INSERT INTO distance_store (idStore1, idStore2, distance) VALUES (1, 10, 5.6);
+
+
